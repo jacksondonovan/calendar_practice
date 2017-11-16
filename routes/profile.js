7a1b1fe798e3ad8f4c622fbdb765ce64dcf168ef
Jackson Donovan <iinventedtwitter@gmail.com>
2017-11-16 12:14:33 -0700

    added assigned to column in bookings

--- routes/profile.js
+++ routes/profile.js
@@ -40,11 +40,46 @@ router.get('/service_provider/:email',(req,res)=>{
         }
       }
       let revenue = bookingsCompleted * 19.99
+      let mostUpcoming;
+
+      var upcomingDate = function(dateSTR){
+        let arrDate = dateSTR.split('/')
+        return arrDate[2]
+      }
+
+      let allDays = []
+      for(let i = 0; i < myBookings.length; i++){
+        allDays.push(upcomingDate(myBookings[i].date_needed))
+      }
+      var bubble = function(arr){
+        let temp
+        let truth = true;
+        while(truth){
+          let counter = 0;
+          for(let i = 0; i < arr.length; i++){
+            if(arr[i+1] < arr[i]){
+              temp = arr[i];
+              arr[i] = arr[i+1];
+              arr[i+1] = temp;
+              counter++;
+            }
+          }
+          if(counter === 0){
+            truth = false;
+          }
+        }
+        return arr;
+      }
+      bubble(allDays)
+
       res.render('service_provider_profile',{
         SOdetails:data,
         totalBookings:myBookings.length,
         completedBookings:bookingsCompleted,
-        displayRevenue:revenue
+        displayRevenue:revenue,
+        nextBooking:allDays[0],
+        secondNextBooking:allDays[1],
+        thirdNextBooking:allDays[2]
       })
     })
   })
