a17437160bffc3d5fe722a36d7fd975b64b91c7c
Jackson Donovan <iinventedtwitter@gmail.com>
2017-11-28 15:16:51 -0700

    checkout complete, sp profile still renders twice

--- routes/profile.js
+++ routes/profile.js
@@ -48,118 +48,119 @@ router.post('/property_owner/:email',(req,res)=>{
 router.get('/service_provider/:email',(req,res)=>{
   console.log(req.params , 'here are the params');
     linkQuery.getServiceProvider().where('email',req.params.email).first().then((data)=>{
-      console.log(data);
-      linkQuery.getMyBookings().where('requested_for',data.company_name).then((myBookings)=>{
-        function upcomingDate(dateSTR){        let arrDate = dateSTR.split('/')
-          return arrDate[2]
-        }
-        var bubble = function(arr){
-          let temp
-          let truth = true;
-          while(truth){
-            let counter = 0;
-            for(let i = 0; i < arr.length; i++){
-              if(arr[i+1] < arr[i]){
-                temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = temp;
-                counter++;
+      if(data){
+        linkQuery.getMyBookings().where('requested_for',data.company_name).then((myBookings)=>{
+          function upcomingDate(dateSTR){        let arrDate = dateSTR.split('/')
+            return arrDate[2]
+          }
+          var bubble = function(arr){
+            let temp
+            let truth = true;
+            while(truth){
+              let counter = 0;
+              for(let i = 0; i < arr.length; i++){
+                if(arr[i+1] < arr[i]){
+                  temp = arr[i];
+                  arr[i] = arr[i+1];
+                  arr[i+1] = temp;
+                  counter++;
+                }
+              }
+              if(counter === 0){
+                truth = false;
               }
             }
-            if(counter === 0){
-              truth = false;
+            return arr;
+          }
+          var justDates = [];
+          var justPendings = [];
+          for(let i = 0; i < myBookings.length; i++){
+            if(!myBookings[i].is_available && !myBookings[i].is_completed){
+              justDates.push(upcomingDate(myBookings[i].date_needed));
+              justPendings.push(myBookings[i])
             }
           }
-          return arr;
-        }
-        var justDates = [];
-        var justPendings = [];
-        for(let i = 0; i < myBookings.length; i++){
-          if(!myBookings[i].is_available && !myBookings[i].is_completed){
-            justDates.push(upcomingDate(myBookings[i].date_needed));
-            justPendings.push(myBookings[i])
-          }
-        }
-        function anotherBubble(pendingarr,pendingdates){
-          let temp;
-          let newtemp;
-          let truth = true;
-          while(truth){
-            let count = 0;
-            for(let i = 0; i < pendingarr.length; i++){
-              if(pendingdates[i] > pendingdates[i+1]){
-                newtemp = pendingdates[i];
-                pendingdates[i] = pendingdates[i+1];
-                pendingdates[i+1] = newtemp;
-                temp = pendingarr[i];
-                pendingarr[i] = pendingarr[i+1];
-                pendingarr[i+1] = temp;
-                count++
+          function anotherBubble(pendingarr,pendingdates){
+            let temp;
+            let newtemp;
+            let truth = true;
+            while(truth){
+              let count = 0;
+              for(let i = 0; i < pendingarr.length; i++){
+                if(pendingdates[i] > pendingdates[i+1]){
+                  newtemp = pendingdates[i];
+                  pendingdates[i] = pendingdates[i+1];
+                  pendingdates[i+1] = newtemp;
+                  temp = pendingarr[i];
+                  pendingarr[i] = pendingarr[i+1];
+                  pendingarr[i+1] = temp;
+                  count++
+                }
+              }
+              if(count === 0){
+                truth = false;
               }
             }
-            if(count === 0){
-              truth = false;
+            return pendingarr;
+          }
+          anotherBubble(justPendings,justDates)
+          let bookingsCompleted = 0;
+          let pendingBookingDates = []
+          for(let i = 0; i < myBookings.length; i++){
+            if(myBookings[i].is_completed){
+              bookingsCompleted++
+            }
+            if(!myBookings[i].is_available && !myBookings[i].is_completed){
+              pendingBookingDates.push(upcomingDate(myBookings[i].date_needed))
             }
           }
-          return pendingarr;
-        }
-        anotherBubble(justPendings,justDates)
-        let bookingsCompleted = 0;
-        let pendingBookingDates = []
-        for(let i = 0; i < myBookings.length; i++){
-          if(myBookings[i].is_completed){
-            bookingsCompleted++
+          let revenue = bookingsCompleted * 19.99
+          if(justPendings[0] && !justPendings[1]){
+            res.render('service_provider_profile',{
+              SOdetails:data,
+              totalBookings:pendingBookingDates.length,
+              completedBookings:bookingsCompleted,
+              displayRevenue:revenue,
+              nextPending:justPendings[0],
+              nextScheduling:upcomingDate(justPendings[0].date_needed)
+            })
           }
-          if(!myBookings[i].is_available && !myBookings[i].is_completed){
-            pendingBookingDates.push(upcomingDate(myBookings[i].date_needed))
+          if(justPendings[0] && justPendings[1] && !justPendings[2]){
+            res.render('service_provider_profile',{
+              SOdetails:data,
+              totalBookings:pendingBookingDates.length,
+              completedBookings:bookingsCompleted,
+              displayRevenue:revenue,
+              nextScheduling:upcomingDate(justPendings[0].date_needed),
+              nextPending:justPendings[0],
+              secondnextPending:justPendings[1],
+              secondNextScheduling:upcomingDate(justPendings[1].date_needed)
+            })
           }
-        }
-        let revenue = bookingsCompleted * 19.99
-        if(justPendings[0] && !justPendings[1]){
-          res.render('service_provider_profile',{
-            SOdetails:data,
-            totalBookings:pendingBookingDates.length,
-            completedBookings:bookingsCompleted,
-            displayRevenue:revenue,
-            nextPending:justPendings[0],
-            nextScheduling:upcomingDate(justPendings[0].date_needed)
-          })
-        }
-        if(justPendings[0] && justPendings[1] && !justPendings[2]){
-          res.render('service_provider_profile',{
-            SOdetails:data,
-            totalBookings:pendingBookingDates.length,
-            completedBookings:bookingsCompleted,
-            displayRevenue:revenue,
-            nextScheduling:upcomingDate(justPendings[0].date_needed),
-            nextPending:justPendings[0],
-            secondnextPending:justPendings[1],
-            secondNextScheduling:upcomingDate(justPendings[1].date_needed)
-          })
-        }
-        if(justPendings[0] && justPendings[1] && justPendings[2]){
-          res.render('service_provider_profile',{
-            SOdetails:data,
-            totalBookings:pendingBookingDates.length,
-            completedBookings:bookingsCompleted,
-            displayRevenue:revenue,
-            nextPending:justPendings[0],
-            nextScheduling:upcomingDate(justPendings[0].date_needed),
-            secondnextPending:justPendings[1],
-            secondNextScheduling:upcomingDate(justPendings[1].date_needed),
-            thirdnextPending:justPendings[2],
-            thirdNextScheduling:upcomingDate(justPendings[2].date_needed)
-          })
-        }
-        if(!justPendings[0]){
-          res.render('service_provider_profile',{
-            SOdetails:data,
-            totalBookings:pendingBookingDates.length,
-            completedBookings:bookingsCompleted,
-            displayRevenue:revenue
-          })
-        }
-      })
+          if(justPendings[0] && justPendings[1] && justPendings[2]){
+            res.render('service_provider_profile',{
+              SOdetails:data,
+              totalBookings:pendingBookingDates.length,
+              completedBookings:bookingsCompleted,
+              displayRevenue:revenue,
+              nextPending:justPendings[0],
+              nextScheduling:upcomingDate(justPendings[0].date_needed),
+              secondnextPending:justPendings[1],
+              secondNextScheduling:upcomingDate(justPendings[1].date_needed),
+              thirdnextPending:justPendings[2],
+              thirdNextScheduling:upcomingDate(justPendings[2].date_needed)
+            })
+          }
+          if(!justPendings[0]){
+            res.render('service_provider_profile',{
+              SOdetails:data,
+              totalBookings:pendingBookingDates.length,
+              completedBookings:bookingsCompleted,
+              displayRevenue:revenue
+            })
+          }
+        })
+      }
     })
 })
 
